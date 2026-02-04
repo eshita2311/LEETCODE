cfccbc881dff3251f4e7e3b227d8afc3c0843006
ESHITA VERMA <eshitaverma11@gmail.com>
2026-02-04 22:19:41 +0530

    Time: 3 ms (94.87%), Space: 95 MB (74.36%) - LeetHub

---
+++ 3640-trionic-array-ii/3640-trionic-array-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+    public long maxSumTrionic(int[] nums) {
+        
+        int n = nums.length;
+        long res= -1 * (long)1e16;
+
+        for(int i=1;i<n-2;i++){
+
+            int a = i; //start
+            int b = i; //end
+
+            long net = nums[a];
+
+            //decrease
+            while(b+1<n && nums[b+1] < nums[b]){
+                net+=(long)nums[b+1];
+                b++;
+            }
+
+            if(b==a)continue;
+
+            int c= b; // store end value
+
+            long left = 0;
+            long right = 0;
+
+            long lx =Integer.MIN_VALUE;
+            long rx =Integer.MIN_VALUE;
+
+            //left increase
+            while(a-1>=0 && nums[a-1] < nums[a]){
+                left+=(long)nums[a-1];
+                lx = Math.max(lx, left);
+                a--;
+            }
+
+            if(a==i)continue;
+
+            //right increase
+            while(b+1<n && nums[b+1] > nums[b]){
+                right+=(long)nums[b+1];
+                rx = Math.max(rx, right);
+                b++;
+            }
+
+            if(b==c)continue;
+            i=b-1;
+            res = Math.max( res, lx+rx+net);
+
+        }
+        return res;
+    }
+}
